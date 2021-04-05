728031268fa4c1836f22ac1dc1fa4a4499c521a4
mkantsurov <mkantsurov@gmail.com>
2021-04-05 18:48:17 +0300

    updated ui

--- src/app/ui/main/main/main.component.ts
+++ src/app/ui/main/main/main.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
 import {AuthenticationService} from "../../../services/authentication.service";
+import {Observable} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {map} from "rxjs/operators";
 
 
 @Component({
@@ -15,7 +18,8 @@ export class MainComponent implements OnInit {
 
   constructor(iconRegistry: MatIconRegistry,
               sanitizer: DomSanitizer,
-              private authService: AuthenticationService) {
+              private authService: AuthenticationService,
+              private breakpointObserver: BreakpointObserver) {
     iconRegistry.addSvgIcon('thumbs-up', sanitizer.bypassSecurityTrustResourceUrl('/assets/images/thumbup-icon.svg'));
 
     this.routeLinks = [
@@ -45,4 +49,8 @@ export class MainComponent implements OnInit {
   ngOnInit() {
   }
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 }
