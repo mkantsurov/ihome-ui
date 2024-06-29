725ceaf84a3422225f03c9751745a3efdcb8ebfe
mkantsurov <mkantsurov@gmail.com>
2024-06-29 14:22:14 +0300

    migrating stuff to angular 17

--- src/app/services/guest.service.ts
+++ src/app/services/guest.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PressureStat} from "../domain/pressurestat";
-import {OutdoorTempStat} from "../domain/outdoor-temp-stat";
-import {PowerVoltage} from "../domain/power-voltage";
+import {Observable} from 'rxjs';
+import {PressureStat} from '../domain/pressurestat';
+import {OutdoorTempStat} from '../domain/outdoor-temp-stat';
+import {PowerVoltage} from '../domain/power-voltage';
 import {PowerVoltageExt} from '../domain/power-voltage-ext';
 import {ExtPowerSummary} from '../domain/ext-power-summary';
 
@@ -34,7 +34,7 @@ export class GuestService {
 
   getExtPowerVoltageStat(): Observable<ExtPowerSummary> {
     console.info('Getting power voltage statistics...');
-    return this.http.get<ExtPowerSummary>(`${this.baseUrl}/power-summary/`)
+    return this.http.get<ExtPowerSummary>(`${this.baseUrl}/power-summary`)
   }
 
 }
