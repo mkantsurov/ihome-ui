03a454d4af46080bfd42cab96462f9718470f64b
mkantsurov <mkantsurov@gmail.com>
2026-04-30 18:06:39 +0300

    version.json

--- src/app/directive/error-view.directive.spec.ts
+++ src/app/directive/error-view.directive.spec.ts
@@ -1,8 +1,12 @@
 import { ErrorViewDirective } from './error-view.directive';
+import { TemplateRef, ViewContainerRef } from '@angular/core';
 
 describe('ErrorViewDirective', () => {
   it('should create an instance', () => {
-    const directive = new ErrorViewDirective();
+    const directive = new ErrorViewDirective(
+      {} as ViewContainerRef,
+      {} as TemplateRef<any>
+    );
     expect(directive).toBeTruthy();
   });
 });
